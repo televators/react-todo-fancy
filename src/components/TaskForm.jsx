01801607602402ac23abb3626f1b0fd0fe74bf38
Justin Higgs <justinphiggs@gmail.com>
2024-04-20 12:36:29 -0400

    Main tasks state and LS effect are still in TaskTracker parent component. Rest of event handlers and newTask state moved into the form and list components. All working.

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,9 +1,42 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import TaskFormButton from "./TaskFormButton";
 
-function TaskForm( { newTask, onChange, onKeyUp, addTask, clearTasks } ) {
+function TaskForm( { tasks, setTasks } ) {
+  const [newTask, setNewTask] = useState( '' );
+
+  function handleInputChange( event ) {
+    const { value } = event.target;
+
+    setNewTask( value );
+  }
+
+  function handleInputEnter( e ) {
+    if ( e.keyCode == '13' || e.key == 'Enter' ) {
+      addTask();
+    }
+  }
+
+  function addTask() {
+    if ( newTask.trim() === '' ) return;
+
+    const newTaskObject = {
+      id: nanoid(),
+      text: newTask.trim()
+    };
+
+    setTasks( [...tasks, newTaskObject] );
+    setNewTask( '' );
+  }
+
+  function clearTasks() {
+    setTasks( [] );
+    setNewTask('');
+  }
+
   return (
     <section className='task-form'>
-      <input type="text" placeholder='Add task' value={ newTask } onChange={ ( e ) => onChange( e ) } onKeyUp={ onKeyUp } />
+      <input type="text" placeholder='Add task' value={ newTask } onChange={ handleInputChange } onKeyUp={ handleInputEnter } />
       <TaskFormButton title={ 'Add' } cssClass={ 'add-button' } clickHandler={ addTask } />
       <TaskFormButton title={ 'Reset' } cssClass={ 'reset-button' } clickHandler={ clearTasks } />
     </section>
--- src/components/TaskTracker.jsx
+++ src/components/TaskTracker.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import TaskForm from './TaskForm';
 import TaskList from './TaskList';
 import EmptyState from './EmptyList';
@@ -10,7 +10,6 @@ const TaskTracker = () => {
     { id: nanoid(), text: 'Do a thing' },
     { id: nanoid(), text: 'Do another thing' },
   ];
-  const [newTask, setNewTask] = useState( '' );
   const [tasks, setTasks] = useState( () => {
     const storedTasks = localStorage.getItem( 'tasks' );
 
@@ -26,93 +25,15 @@ const TaskTracker = () => {
       console.error( 'Failed to save tasks to LocalStorage:', error );
     }
   }, [tasks] );
-
-  function handleInputChange( event ) {
-    const { value } = event.target;
-
-    setNewTask( value );
-  }
-
-  function handleInputEnter( e ) {
-    if ( e.keyCode == '13' || e.key == 'Enter' ) {
-      addTask();
-    }
-  }
-
-  function addTask() {
-    if ( newTask.trim() === '' ) return;
-
-    const newTaskObject = {
-      id: nanoid(),
-      text: newTask.trim()
-    };
-
-    setTasks( [...tasks, newTaskObject] );
-    setNewTask( '' );
-  }
-
-  function clearTasks() {
-    setTasks( [] );
-    setNewTask('');
-  }
-
-  function deleteTask( index ) {
-    const updatedTasks = tasks.filter( task => task.id !== index );
-
-    setTasks( updatedTasks );
-  }
-
-  function moveTaskUp( id ) {
-    let item_index = -1;
-
-    // Could do Array.findIndex, Array.find, or Array.indexOf but a for loop is more performant.
-    for ( let i = 0; i < tasks.length; i++ ) {
-      if ( tasks[i].id === id ) {
-        item_index = i;
-
-        break;
-      }
-    }
-
-    // Bail if already top item or ID doesn't exist in tasks for some reason.
-    if ( ( item_index === -1 ) || ( item_index === 0 ) ) return;
-
-    const updatedTasks = [...tasks];
-
-    // Swap position of this item and item above it in the list.
-    [updatedTasks[item_index], updatedTasks[item_index - 1]] = [updatedTasks[item_index - 1], updatedTasks[item_index]];
-    setTasks( updatedTasks );
-  }
-
-  function moveTaskDown( id ) {
-    let item_index = -1;
-
-    // Could do Array.findIndex, Array.find, or Array.indexOf but a for loop is more performant.
-    for ( let i = 0; i < tasks.length; i++ ) {
-      if ( tasks[i].id === id ) {
-        item_index = i;
-
-        break;
-      }
-    }
-
-    // Bail if already bottom item or ID doesn't exist in tasks for some reason.
-    if ( ( item_index === -1 ) || ( item_index === ( tasks.length - 1 ) ) ) return;
-
-    const updatedTasks = [...tasks];
-    // Swap position of this item and item below it in the list.
-    [updatedTasks[item_index], updatedTasks[item_index + 1]] = [updatedTasks[item_index + 1], updatedTasks[item_index]];
-    setTasks( updatedTasks );
-  }
   //#endregion
 
   return (
     <>
-      <TaskForm newTask={ newTask } onChange={ handleInputChange } onKeyUp={ handleInputEnter } addTask={ addTask } clearTasks={ clearTasks } />
+      <TaskForm tasks={ tasks } setTasks={ setTasks } />
 
       {
         tasks.length
-          ? <TaskList tasks={ tasks } handleDelete={ deleteTask } handleMoveUp={ moveTaskUp } handleMoveDown={ moveTaskDown } />
+          ? <TaskList tasks={ tasks } setTasks={ setTasks } />
           : <EmptyState />
       }
     </>
